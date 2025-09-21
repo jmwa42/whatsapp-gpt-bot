4b9c25062898427b3d78932e9b8c4fc27449dfe2
jmwa200 <jmwa200@gmail.com>
2025-09-21 14:20:34 +0300

    dashboard9

--- index.js
+++ index.js
@@ -22,10 +22,6 @@ import {
 } from './bot/storage.js';
 import { stkPush } from './bot/mpesa.js';
 
-app.set("view engine", "ejs");
-app.set("views", "./dashboard/views");
-app.use(expressLayouts);
-app.set("layout", "layout"); // default layout file: views/layout.ejs
 
 const { Client, LocalAuth } = pkg;
 
@@ -49,6 +45,8 @@ app.set('views', path.join(__dirname, 'dashboard', 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts);
+app.set("layout", "layout"); // default layout file: views/layout.ejs
 
 // helper local-logging functions (for dashboard fallback)
 function appendJson(file, obj) {
